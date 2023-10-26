ab5b8bcf3fe725215f297c2480a77c5e3964ea22
Lauren Mason <lmason@squareup.com>
2023-10-26 13:52:56 -0400

    Completed lesson 3.3

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+
+	var colours [3]string
+	colours[0] = "Red"
+	colours[1] = "Green"
+	colours[2] = "Blue"
+	fmt.Println(colours)
+	fmt.Println(colours[0])
+
+	var numbers = [5]int{5, 3, 1, 2, 4}
+	fmt.Println(numbers)
+
+	fmt.Println("Number of colours:", len(colours))
+	fmt.Println("Number of numbers:", len(numbers))
 }
